47d9ecbed6918eb462ce2e6a0903ad60d444222e
vikaschaudhary23 <vikaschaudhary2309@gmail.com>
2024-02-20 22:34:53 +0530

    Commiting

---
+++ SnakeGame/Snakegame.java
@@ -0,0 +1,23 @@
+package SnakeGame;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Snakegame extends JFrame {
+    Snakegame(){
+        super("Snake Game");
+        add(new Board());
+        pack();
+
+//        setSize(300,300);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Snakegame();
+    }
+
+}
